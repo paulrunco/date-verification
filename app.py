2cdb2d85d15226ce9e2f552481cb7dfb67f5f57c
Paul Runco <runco.paul@gmail.com>
2022-05-20 14:22:45 -0400

    Connect UI to Report Update Script
    - add update report function when update button is clicked
    - add feedback to update_button text
    - fix date formatting
    - add clear form function callable by shortcut or menu

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from os import path
-from tkinter import Label, Entry, Button, Menu, StringVar, filedialog, Tk, END
+from tkinter import BooleanVar, Label, Entry, Checkbutton, Button, Menu, StringVar, filedialog, Tk, END
 from tkinter import messagebox as mb
-import tkinter
 import webbrowser
 
-from numpy import pad
-
 import settings
+import functions
 
 class App(Tk):
     def __init__(self):
@@ -17,7 +15,7 @@ class App(Tk):
         path_to_icon = path.abspath(path.join(path.dirname(__file__), 'icon.ico'))
         self.iconbitmap(path_to_icon)
         self.title('Date Verification Utility')
-        self.version = "0.1.0"
+        self.version = "1.0.0"
         self.author = "Paul Runco"
 
         self.settings = settings.load()
@@ -26,8 +24,10 @@ class App(Tk):
         self.menubar = Menu(self)
 
         self.filemenu = Menu(self.menubar, tearoff=False)
-        self.filemenu.add_command(label="Exit", command=self.quit, accelerator="Ctrl+q")
-        self.filemenu.add_command(label="Settings", command=lambda: self.display_settings(app, self.settings), accelerator="Ctrl+.")
+        # self.filemenu.add_command(label="Settings", command=lambda: self.display_settings(self), accelerator="Ctrl+.")
+        self.filemenu.add_command(label="Clear", command=self.clear_form, accelerator="Ctrl+Delete")
+        self.filemenu.add_separator()
+        self.filemenu.add_command(label="Exit", command=self.quit, accelerator="Ctrl+Q")
         self.menubar.add_cascade(label="File", menu=self.filemenu)
 
         self.helpmenu = Menu(self.menubar, tearoff=False)
@@ -37,7 +37,7 @@ class App(Tk):
 
         self.bind_all("<Control-q>", exit)
         self.bind_all("<Control-?>", self.open_docs)
-        self.bind_all("<Control-.>", self.display_settings)
+        self.bind_all("<Control-Delete>", self.clear_form)
 
         self.config(menu=self.menubar)
 
@@ -46,43 +46,71 @@ class App(Tk):
         self.estimated_completion_label = Label(
             self,
             text="Estimated Completion Dates",
-            ).pack(padx=(5,0), anchor='w')
+            ).grid(row=0, column=0, padx=(5,0), columnspan=2, sticky='w')
         # Entry
         self.estimated_completion_entry = Entry(
             self, 
             width=80, 
             textvariable=StringVar
             )
-        self.estimated_completion_entry.pack(padx=5, pady=(0, 5))
+        self.estimated_completion_entry.grid(row=1, column=0, padx=5, pady=(0, 5), columnspan=2, sticky='w')
         # Button
         self.estimated_completion_button = Button(
             self, text="Browse", 
             command=lambda: self.browse_for("estimated_completion")
-            ).pack(padx=5, pady=(0, 5), anchor='e')
+            ).grid(row=1, column=2, padx=5, pady=(0, 5), sticky='w')
 
         ## Promise Date Verification Template Entry
         # Label
         self.date_verification_label = Label(
             self,
             text="Date Verification Template"
-            ).pack(padx=(5,0), anchor='w')
+            ).grid(row=2, column=0, padx=(5,0), columnspan=2, sticky='w')
         # Entry
         self.date_verification_entry = Entry(
             self,
             width=80,
             textvariable=StringVar
             )
-        self.date_verification_entry.pack(padx=5, pady=(0, 5))
+        self.date_verification_entry.grid(row=3, column=0, padx=5, pady=(0, 5), columnspan=2, sticky='w')
         # Button
         self.date_verification_button = Button(
-            self, text="Browse", 
+            self, 
+            text="Browse", 
             command=lambda: self.browse_for("date_verification")
-            ).pack(padx=5, pady=(0, 5), anchor='e')
+            ).grid(row=3, column=2, padx=5, pady=(0, 5), sticky='w')
 
-        self.generate_button = Button(
+        ## Settings
+        self.transit_time_label = Label(
+            self,
+            text="Transit Time Days",
+            ).grid(row=4, column=0, sticky='e')
+
+        self.transit_days = StringVar(value=self.settings['ShippingOptions']['TransitDays'])
+        self.transit_days.trace_add('write', self.save_settings)
+        self.transit_time_entry = Entry(
+            self,
+            width=4, 
+            textvariable=self.transit_days,
+            justify='center'
+            )
+        self.transit_time_entry.grid(row=4, column=1, sticky='w')
+
+        self.include_weekends = BooleanVar(value=self.settings['ShippingOptions']['IncludeWeekends'])
+        self.include_weekends_checkbox = Checkbutton(
+            self,
+            text="Include Weekends",
+            variable=self.include_weekends,
+            onvalue=True, 
+            offvalue=False,
+            command=self.save_settings
+        ).grid(row=4, column=1)
+
+        self.update_button = Button(
             self, text="Update report",
-            command=lambda: self.on_click_update()
-        ).pack(fill='x', padx=5, pady=5)
+            command=self.on_click_update
+        )
+        self.update_button.grid(row=5, padx=5, pady=5, columnspan=3, sticky='ew')
 
     def browse_for(self, target):
         file_name = filedialog.askopenfilename(
@@ -97,6 +125,8 @@ class App(Tk):
             self.date_verification_entry.delete(0, END)
             self.date_verification_entry.insert(0, file_name)
 
+        self.reset_button()
+
     def on_click_update(self):
         path_to_estimated_completion = self.estimated_completion_entry.get()
         if path_to_estimated_completion == "":
@@ -110,23 +140,35 @@ class App(Tk):
             self.date_verification_entry.config(background='red')
             return
 
-    def display_settings(self, event=None):
-        settings = self.settings
-        settings_window = tkinter.Toplevel(self)
-        settings_window.title('Settings')
+        transit_days = int(self.transit_time_entry.get())
+        include_weekends = self.include_weekends.get()
+        if transit_days == "":
+            mb.showwarning(title='Missing values', message='Please enter a number for Transit Days')
+            self.transit_time_entry.config(background='red')
+            return
 
-        self.transit_time_label = Label(
-            settings_window,
-            text="Transit Time Days",
-            ).grid(row=0, padx=(5,0))
+        self.update_button.configure(text = 'Working...')
+        functions.update_report(path_to_date_verification, path_to_estimated_completion, transit_days, include_weekends)
+        self.update_button.configure(text = 'Done!')
 
-        self.transit_time_entry = Entry(
-            settings_window, 
-            width=20, 
-            textvariable=StringVar,
-            )
-        self.transit_time_entry.grid(row=0, column=1, padx=(0, 5))
-        self.transit_time_entry.insert(0, settings['ShippingOptions']['TransitDays'])
+
+    def save_settings(self, *args):
+        transit_days = self.transit_days.get()
+        include_weekends = self.include_weekends.get()
+        if transit_days:
+            self.settings['ShippingOptions'] = {'TransitDays': transit_days, 'IncludeWeekends': include_weekends}
+        else:
+            self.settings['ShippingOptions'] = {'TransitDays': 3, 'IncludeWeekends': include_weekends}
+        settings.save(self.settings)
+        self.reset_button()
+
+    def reset_button(self):
+        self.update_button.configure(text = 'Update report')
+
+    def clear_form(self, *args):
+        self.estimated_completion_entry.delete(0, END)
+        self.date_verification_entry.delete(0, END)
+        self.reset_button()
 
     def open_about(self):
         mb.showinfo(
--- functions.py
+++ functions.py
@@ -1,68 +1,50 @@
 import pandas as pd
 
+def update_report(path_to_date_verification, path_to_estimated_completion, transit_days, include_weekends):
 
-path_to_estimated_completion_dates = 'sandbox/SpaceX estimated completion 4-27-22.xlsx'
-path_to_promise_date_verification = 'sandbox/Empty_Promise_Date_Verification_042422 - Copy.xlsx'
+    ## Make a dataframe of the requested lines to update
+    promise_date = pd.DataFrame(pd.read_excel(
+        path_to_date_verification,
+        header=1,
+        sheet_name='Requested Updates'
+        ))
 
-## Make a dataframe of the requested lines to update
-promise_date = pd.DataFrame(pd.read_excel(
-    path_to_promise_date_verification,
-    header=1,
-    sheet_name='Requested Updates'
-    ))
+    ## Make a dataframe of the input data
+    estimated_date = pd.DataFrame(pd.read_excel(
+        path_to_estimated_completion,
+        header=0,
+        sheet_name=0,
+        usecols='A:H'
+        )).set_index(['PO', 'Line'])
 
-## Make a dataframe of the input data
-estimated_date = pd.DataFrame(pd.read_excel(
-    path_to_estimated_completion_dates,
-    header=0,
-    sheet_name=0,
-    usecols='A:H'
-    )).set_index(['PO', 'Line'])
+    ## Look up estimated completion dates from scheduling sheet
+    promise_date = promise_date.join(
+        estimated_date['Estimated Ship Date'],
+        on=['PO #', 'Line #'])
 
-## Look up estimated completion dates from scheduling sheet
-promise_date = promise_date.join(
-    estimated_date['Estimated Ship Date'],
-    on=['PO #', 'Line #'])
+    # Update promise dates
+    if include_weekends:
+        promise_date['Verified Current Promise Date'] = promise_date['Estimated Ship Date'] + pd.tseries.offsets.DateOffset(transit_days)
+    else:
+        promise_date['Verified Current Promise Date'] = promise_date['Estimated Ship Date'] + pd.tseries.offsets.BusinessDay(transit_days)
 
-# Update promise dates
-promise_date['Verified Current Promise Date'] = promise_date['Estimated Ship Date'] + pd.tseries.offsets.BusinessDay(3)
-print(promise_date)
+    promise_date.drop_duplicates(['PO #', 'Line #'], keep="first", inplace=True)
+    promise_date['Verified Current Promise Date'] = promise_date['Verified Current Promise Date'].dt.date
 
-
-
-
-# ## Write promise dates into the Promise Date Verification template
-# with pd.ExcelWriter(
-#     path_to_promise_date_verification,
-#     mode='a',
-#     if_sheet_exists='overlay',
-#     date_format='yyyy-mm-dd',
-#     datetime_format='yyyy-mm-dd'
-# ) as writer:
-#     test.to_excel(
-#         writer,
-#         sheet_name='Requested Updates',
-#         startrow=2,
-#         startcol=7,
-#         header=False,
-#         index=False
-#     )
-
-
-
-
-## Look up each order-line in ETC data and...
-#   if exists:
-#
-#       if new date != old date:
-#           update date with new date + 3 days
-#   else:
-#       flag for user review
-
-
-## Format and write output data to sheet
-
-
-
-## Manage settings file
-# load settings file if exists, otherwise create new
+    ## Write promise dates into the Promise Date Verification template
+    print(promise_date)
+    with pd.ExcelWriter(
+        path_to_date_verification,
+        mode="a",
+        if_sheet_exists="overlay",
+        date_format="YYYY-MM-DD",
+        datetime_format="YYYY-MM-DD"
+    ) as writer:
+        promise_date['Verified Current Promise Date'].to_excel(
+            writer,
+            sheet_name="Requested Updates",
+            startrow=2,
+            startcol=7,
+            header=False,
+            index=False
+        )
